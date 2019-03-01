a12194741fc8610650b954540cad6ee478dc4573
admin <you@example.com>
2019-03-01 17:36:33 +0545

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-
+const PORT =  process.env.PORT || 3000;
 
 let server = http.createServer(app);
 
-server.listen(3000);
+server.listen(PORT);
